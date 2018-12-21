8b862e984a74738c94919e14359b2143d039cddf
yanistvg <yanisgenytv@gmail.com>
2018-12-21 11:25:04 +0100

    ajout fichier 2

---
+++ 2/ex00.c
@@ -0,0 +1,5 @@
+// creer la fonction ft_putchar 
+// prototiper void ft_putchar(char c);
+// utiliser la fonction write() pour ecrire
+// printf() est interdit
+// creer un main pour tester la fonction
